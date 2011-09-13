1916350239ef56ecee82590636c7bdfab00c91d4
jk <jens.kohl@gmail.com>
2011-09-13 10:04:29 +0200

    Added a grayscale category to UIImage

---
+++ UIImage+grayscale.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+grayscale.h
+//  JobStairs
+//
+//  Created by Jens Kohl on 12.09.11.
+//  Copyright (c) 2011 milch & zucker AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (grayscale)
+- (UIImage *)convertToGrayscale;
+@end
